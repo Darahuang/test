e8b7b0dec7559dd0539affc3650dab9c5bc4ff37
darahuang <staceyhuang1204@gmail.com>
2021-06-08 02:57:23 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    {{ name}}
+  </div>
+  <router-view/>
+
+  <button type="button" class="btn btn-primary">primary</button>
+</template>
+
+<style lang="scss">
+@import "bootstrap"
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      name: process.env.VUE_APP_APIPATH,
+    };
+  },
+};
+</script>
